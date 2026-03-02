336610ffd37b47e3a9251bbaf77beda6b228e055
voidapex11 <154328367+voidapex11@users.noreply.github.com>
2026-03-02 09:22:28 +0000

    Quaternion processing

--- data_proccessing/data_proccessing.py
+++ data_proccessing/data_proccessing.py
@@ -1,9 +1,28 @@
 import random
 import csv
 import numpy as np
+import quaternion
 import math
 
+from quaternion import angular_velocity
+
+
 # target: https://docs.google.com/document/d/1ZrHmUzFSbimNmvZLT4MfxJwNgPxCKA-LfHc2wqpyyek/edit?tab=t.0
+
+def weighted_moving_average(x,y,step_size=0.05,width=1):
+    bin_centers  = np.arange(np.min(x),np.max(x)-0.5*step_size,step_size)+0.5*step_size
+    bin_avg = np.zeros(len(bin_centers))
+
+    #We're going to weight with a Gaussian function
+    def gaussian(x,amp=1,mean=0,sigma=1):
+        return amp*np.exp(-(x-mean)**2/(2*sigma**2))
+
+    for index in range(0,len(bin_centers)):
+        bin_center = bin_centers[index]
+        weights = gaussian(x,mean=bin_center,sigma=width)
+        bin_avg[index] = np.average(y,weights=weights)
+
+    return (bin_centers,bin_avg)
 
 def slice_data_by_time_example(array,min,max):
     b=array[(min<=array[:,1])&(array[:,1]<max)]
@@ -60,9 +79,13 @@ def inverse_barametric_formula(P,P_b, T, R_Star=8.31432*10**3, g=9.8, M=28.9644,
     return height
 
 def diferenciate(x_list, y_list):
-    return np.diff(y_list)/np.diff(x_list)
-
-
+    """
+    Aproximates the dy/dx
+    :param x_list:
+    :param y_list:
+    :return:
+    """
+    return np.gradient(y_list,x_list)
 
 def proces_radio(temperature,pressure,time):
     temperature=np.array(temperature)
@@ -76,6 +99,22 @@ def load_from_csv(path):
     with open(path, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         return list(reader)
+
+def process_quaternions(abs_orientation_array,time):
+    """
+    Calculates angular displacement, velocity and acceleration from absolute orientation.
+    :param abs_orientation_array: an aray of tuples of quaternions
+    :param time: time that each quaternion was recorded
+    :return:
+    """
+    quat_array = quaternion.as_quat_array(abs_orientation_array)
+    starting_angle = np.quaternion(abs_orientation_array[0])
+    angular_displacement_quat = quat_array-starting_angle
+    angular_velocity_quat = diferenciate(angular_displacement_quat,time)
+    angular_acceleration_quat = diferenciate(angular_velocity_quat,time)
+    return angular_displacement_quat, angular_velocity_quat, angular_acceleration_quat
+
+
 
 for i in range(1,10):
     a = random.randint(1,10)
